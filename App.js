2436401349e129244b65cf1add44424bf70a5055
Dhruv kushwah <dhruvkushwah243@gmail.com>
2025-03-20 16:59:24 +0530

    "[Dhruv] Add. Added UC2"

--- App.js
+++ App.js
@@ -2,3 +2,23 @@
 const PRESENT_THRESHOLD = 0.5;
 let status = Math.random() >= PRESENT_THRESHOLD ? "Present" : "Absent";
 console.log(status);
+
+//UC2 - Calculate Daily Wage based on part time or full time
+const WAGE_PER_HOUR = 20;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+
+let empHours;
+switch (Math.floor(Math.random() * 3)) {
+    case 1:
+        empHours = PART_TIME_HOURS;
+        break;
+    case 2:
+        empHours = FULL_TIME_HOURS;
+        break;
+    default:
+        empHours = 0;
+}
+
+let dailyWage = empHours * WAGE_PER_HOUR;
+console.log(`Daily Wage: $${dailyWage}`);
